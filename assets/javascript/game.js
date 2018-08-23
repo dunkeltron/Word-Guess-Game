96e6b01db6b1cf2aafad9717b80338a63242a72a
Ian Dunkelberg <idunkelberg@gmail.com>
2018-08-22 20:56:24 -0700

    added lettersGuessed and partial lettersRevealed console log functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
 
-
+// still need to display word length with "_"
+//still need to display letters guessed
+//still need to make layout of site
 var wordList = ["banana", "apple", "pineapple", "pear", "peach", "watermelon", "orange", "lemon", "lime"];
 var listIndex;
 var curWord;
@@ -11,21 +13,23 @@ var losses = 0;
 var lettersRevealed = [];
 var wrongGuessesRemaining = 5;
 resetGame();
-document.onkeyup = (function (event) {
 
+displayPartial(lettersRevealed);
+displayGuesses(lettersGuessed);
+document.onkeyup = (function (event) {
     curGuess = event.key.toLowerCase();
     if (isAlphaOrParen(curGuess)) {
         if (lettersGuessed.indexOf(curGuess) < 0) {
-
             lettersGuessed.push(curGuess);
             if (guessInWord(curGuess)) {
                 lettersRevealed.push(curGuess);
-                console.log(lettersRevealed);
+                //console.log(lettersRevealed);
                 lettersInWord.splice(lettersInWord.indexOf(curGuess), 1);
+                //displayLetters(lettersRevealed);
                 //console.log("lIW= " + lettersInWord);
                 if (lettersInWord.length == 0) {
                     //display winner
-                    console.log("winner");
+                    console.log("You correctly guessed the word " + curWord + "!");
                     wins++;
                     console.log("Wins: " + wins + " Losses: " + losses);
                     resetGame();
@@ -36,12 +40,16 @@ document.onkeyup = (function (event) {
                 console.log(lettersGuessed);
                 if (wrongGuessesRemaining < 0) {
                     //display loser
-                    console.log("loser");
+                    console.log("Unlucky! The word was " + curWord + ".");
                     losses++;
                     console.log("Wins: " + wins + " Losses: " + losses);
                     resetGame();
                 }
             }
+
+            displayPartial(lettersRevealed);
+            //console.log(lettersRevealed);
+            displayGuesses(lettersGuessed);
         }
         else {
             console.log("already guessed " + curGuess);
@@ -70,6 +78,27 @@ function uniqueLetters(str) {
         }
     }
 }
+function displayGuesses(arr) {
+    var str = "";
+    for (i = 0; i < arr.length; i++) {
+        str = str + arr[i];
+
+    }
+    console.log("Letters Guessed: " + str);
+}
+function displayPartial(arr) {
+    var str = "";
+    for (i = 0; i < curWord.length; i++) {
+        if (arr.indexOf(curWord[i]) >= 0) {
+            str = str + curWord[i];
+        }
+        else {
+            str = str + "_";
+        }
+    }
+    console.log(str);
+}
+
 function resetGame() {
     console.log("welcome to word-guess-game");
     //console.log(wordList.length);
